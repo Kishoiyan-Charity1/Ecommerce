bbb0f4355a2fa493e280e19c38f059824961535a
Kishoiyan-Charity1 <charity.kishoiyancharity@gmail.com>
2023-12-12 12:31:34 +0300

    changes

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        {/* <div className="descriptiobox-navigator"> */}
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (123)</div>
+
+            </div>
+            <div className="descriptionbox-description">
+                <p>ertyuiopswertyuiopasdfghjklzxcvbnm,ytyteriobnvnioed9reiojknmfyuiopewwrfnmirohetjbfdvndfghjkl</p>
+                <p>
+                    xcgvhjkl;cvbhnmdcuhjfn mrfvnmclproiuthgfj;lweoirufjkldfitugfjkuigthfjndktgjnfmdkjgunbfmkjfhgbvnmjfjg'cxvbnm,.'
+                </p>
+            </div>
+        {/* </div> */}
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,19 +1,25 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import {useParams} from 'react-router-dom'
-import all_product from '../Components/Assets/all_product';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDispaly from '../Components/ProductDisplay/ProductDispaly';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 
 const Product = () => {
 
-  const {all_products}= useContext(ShopContext);
+  const {all_product}= useContext(ShopContext);
   const {productId}= useParams();
-  const product = all_product.find((e)=> e.id === productId)
+  const product = all_product.find((e)=> e.id === Number(productId));
 
 
   return (
     <div>
-      
+      <Breadcrum product={product}/>
+      <ProductDispaly product={product}/>
+      <DescriptionBox/>
+      <RelatedProducts/>
     </div>
   )
 }
